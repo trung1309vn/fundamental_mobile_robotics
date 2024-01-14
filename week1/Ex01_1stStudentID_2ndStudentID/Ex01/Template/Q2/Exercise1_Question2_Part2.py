743f244a7eb2291b91cb57d4d17a83ac26b759a0
Phuoc Trung Nguyen Ly <phuoctrung1309vn@gmail.com>
2024-01-14 15:27:15 +0200

    Update exercise 2 part 2

--- week1/Ex01_1stStudentID_2ndStudentID/Ex01/Template/Q2/Exercise1_Question2_Part2.py
+++ week1/Ex01_1stStudentID_2ndStudentID/Ex01/Template/Q2/Exercise1_Question2_Part2.py
@@ -1,15 +1,42 @@
 """_NOTE_
 For this part, we do not have any template. You may need some python library to read the csv file.
+
 you have 10,000 random numbers.
+
 obtain the range of the given numbers. 
+
 divide the range into "n" intervals. For instance 50.
+
 for each interval, count the number of random numbers which are between that interval. 
+
 For each interval, you have a number showing the abundance of the random numbers withing that interval. 
-Normalize the distribution of the random varialbe --the 50 numbers you obtianed-- by dividing them to the 1,000.
+
+Normalize the distribution of the random varialbe--the 50 numbers you obtianed--by dividing them to the 1,000.
+
 draw the bar chart. (x is the middle of the interval and y is the normalized number you derived.)
+
 """
 import numpy as np
-data = np.asarray(np.genfromtxt('discrete_random_variable.csv', delimiter=','))
-range = (np.min(data), np.max(data))
+import matplotlib.pyplot as plt
 
+data = np.genfromtxt("discrete_random_variable.csv", delimiter=",")
+n = 50
+x_lin = np.linspace(np.min(data), np.max(data)+0.0000001, n+1)
+mean_lists = []
+norm_lists = []
+
+# For each interval
+for i in range(n):
+    # Get number from an interval
+    values = data[(data >= x_lin[i]) & (data < x_lin[i+1])]
+    # Get number of element in current interval
+    num_val = len(values)
+
+    # Get mean
+    mean_val = np.mean(values)
+    print(mean_val)
+    mean_lists.append(mean_val)
+    norm_lists.append(mean_val/1000.0)
+
+plt.hist(mean_lists, norm_lists)
 
